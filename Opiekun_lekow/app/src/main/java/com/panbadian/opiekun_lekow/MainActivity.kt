087a32f3c445ead181f80d717ce71c51d60688a5
panka134 <134panka@gmail.com>
2022-04-10 12:51:19 +0200

    Add opiekun_lekow screens

---
+++ Opiekun_lekow/app/src/main/java/com/panbadian/opiekun_lekow/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.panbadian.opiekun_lekow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
