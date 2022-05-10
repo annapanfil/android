f3ef6d3e2b7fbacb859493aee022c34f52416a41
panka134 <134panka@gmail.com>
2022-05-10 16:45:33 +0200

    Add add medicine layout

---
+++ Opiekun_lekow/app/src/main/java/com/panbadian/opiekun_lekow/activity_add_med.kt
@@ -0,0 +1,11 @@
+package com.panbadian.opiekun_lekow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_add_med : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_med)
+    }
+}
