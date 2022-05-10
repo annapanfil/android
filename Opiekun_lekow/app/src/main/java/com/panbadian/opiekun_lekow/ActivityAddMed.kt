4ef612693918ae5fb3d142f170a4e40bba4c638d
panka134 <134panka@gmail.com>
2022-05-10 17:02:08 +0200

    Add add medicine to list layout

--- Opiekun_lekow/app/src/main/java/com/panbadian/opiekun_lekow/ActivityAddMed.kt
+++ Opiekun_lekow/app/src/main/java/com/panbadian/opiekun_lekow/ActivityAddMed.kt
@@ -3,7 +3,7 @@ package com.panbadian.opiekun_lekow
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class activity_add_med : AppCompatActivity() {
+class ActivityAddMed : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_med)
---
+++ Opiekun_lekow/app/src/main/java/com/panbadian/opiekun_lekow/ActivityAddMedToList.kt
@@ -0,0 +1,11 @@
+package com.panbadian.opiekun_lekow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityAddMedToList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_med_to_list)
+    }
+}
