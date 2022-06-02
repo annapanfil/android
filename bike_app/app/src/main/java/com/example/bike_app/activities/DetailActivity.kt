b3076941700181e0559c8a83dc63e4404a1950e6
panka134 <134panka@gmail.com>
2022-06-03 00:20:11 +0200

    Add lost file

---
+++ bike_app/app/src/main/java/com/example/bike_app/activities/DetailActivity.kt
@@ -0,0 +1,24 @@
+package com.example.bike_app.activities
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bike_app.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        //Ustawiamy pasek narzędzi jako pasek aplikacji aktywności
+//        val toolbar  = findViewById<Toolbar>(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+//        val actionBar = supportActionBar
+//        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val frag = supportFragmentManager.findFragmentById(R.id.f_details) as RouteDetailFragment
+        val routeId = intent.getLongExtra("route_id", -1)
+        frag.setTrackId(routeId)
+    }
+}
